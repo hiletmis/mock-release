5b421606a8eb8b42d9ebf1bda46773b3ac4f90dd
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-03-14 10:48:32 +0300

    Add prettier

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-
-
-
 export const randomNum = (min: number, max: number) => {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
