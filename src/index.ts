8dd7a22e4cc5af685b214bba388f55666cef037d
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-03-14 14:12:56 +0300

    Modify randomNum

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
 export const randomNum = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+    const minNum = Math.ceil(min);
+    const maxNum = Math.floor(max);
+    const random = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
+    return random;
 };
