5832e822716376729ee592614fd6e1a178e8b7e8
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-03-18 14:08:24 +0300

    Commit gap test 1

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export const randomNum = (min: number, max: number) => {
     const maxNum = Math.floor(max);
     return Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum;
 };
+
+export const one = 1;
