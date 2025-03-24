97ef275df2438f5f49f061d4bf9ef977793a4f52
hiletmis <31757771+hiletmis@users.noreply.github.com>
2025-03-24 10:39:04 +0300

    fix: update gh-pages scripts to generate and verify random number JSON

--- scripts/verify-pages.ts
+++ scripts/verify-pages.ts
@@ -1,7 +1,28 @@
+import * as fs from 'fs';
+import * as path from 'path';
 
-function main() {
-    console.log('Hello World');
+async function main() {
+  const url = 'https://hiletmis.github.io/mock-release/market/random-number.json';
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+
+  const json = await response.json();
+  console.log(json);
+
+  const randomNumber = json.randomNumber;
+
+  const randomNumberFromFile = fs.readFileSync(path.join(__dirname, `../gh-pages/market/random-number.json`), 'utf8');
+  const randomNumberJson = JSON.parse(randomNumberFromFile);
+  const randomNumberFromFileValue = randomNumberJson.randomNumber;
+
+  if (randomNumber === randomNumberFromFileValue) {
+    console.log('The random numbers match!');
+  } else {
+    console.log('The random numbers do not match!');
+  }
 }
-
 
 main();
